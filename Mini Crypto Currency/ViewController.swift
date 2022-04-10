9ba26ef2bc59134eb259fdc0eb909dca50199072
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-04-10 22:03:55 +0700

    Initial Commit

---
+++ Mini Crypto Currency/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mini Crypto Currency
+//
+//  Created by TheGIZzz on 10/4/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
